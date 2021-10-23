b6963089a638e743e684925ace08e097adadb7d7
MaurMurry <rimcyberup@gmail.com>
2021-10-23 17:30:22 +0300

    test commit

--- app/src/components/Saidbar/Saidbar.jsx
+++ app/src/components/Saidbar/Saidbar.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import style from "../CSS/main.module.css";
 
-const Saidbar = (props) =>{
-    return (
-        <div className={style.saidbar}>
-            <ul>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Message</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li><br/>
-                <li><a href="#">Setting</a></li>
-            </ul>
-        </div>
-    )
-}
+const Saidbar = (props) => {
+  return (
+    <div className={style.saidbar}>
+      <ul>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+        <li>
+          <a href="#">Message</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Music</a>
+        </li>
+        <br />
+        <li>
+          <a href="#">Setting</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Saidbar;
