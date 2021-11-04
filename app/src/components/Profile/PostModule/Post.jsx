9007ad7d2b0b4dc0830cca62c5e51c92a3f08711
MaurMurry <rimcyberup@gmail.com>
2021-11-04 21:52:59 +0200

    fix correct

--- app/src/components/Profile/PostModule/Post.jsx
+++ app/src/components/Profile/PostModule/Post.jsx
@@ -6,7 +6,7 @@ const Post = (props) => {
     <div className={style.wrapperPost}>
       <img
         src="https://png.pngtree.com/png-vector/20191116/ourlarge/pngtree-businessman-avatar-icon-vector-download-vector-user-icon-avatar-silhouette-social-png-image_1991050.jpg"
-        alt=""
+        alt="aaa"
       />
       <p>{props.text}</p>
     </div>
